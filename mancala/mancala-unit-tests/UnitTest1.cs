a36e2e44d3fcf322b8342efdaba0ab500da37702
Samuel Christensen <samachr@gmail.com>
2015-10-29 15:58:15 -0600

    initial project commits

---
+++ mancala/mancala-unit-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mancala_unit_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
